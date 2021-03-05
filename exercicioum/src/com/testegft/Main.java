155f6f242077e3b7c17a211a85d5d42386a54338
Eduardo Piorini <eduardopiorini@hotmail.com>
2021-03-05 16:28:16 -0300

    finish

--- exercicioum/src/com/testegft/Main.java
+++ exercicioum/src/com/testegft/Main.java
@@ -1,11 +1,21 @@
 
 package com.testegft;
+import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) throws Exception {
-	InvestimentoComIR investimento = new InvestimentoComIR(1000.00, 0.04);
+    public static void main(String[] args) {
+	Scanner ler = new Scanner(System.in);
 
-	System.out.println(investimento.toString());
+	System.out.println("Valor inicial:");
+	double valorInicial = ler.nextDouble();
+
+	System.out.println("Juros mensais (em decimal): ");
+	double jurosMensais = ler.nextDouble();
+
+	Investimento investimento = new InvestimentoSemIr(valorInicial, jurosMensais);
+	Investimento investimentoComIr = new InvestimentoComIR(valorInicial, jurosMensais);
+
+	//faltou imprimir dados organizdos e tratar exception
   }
 }
